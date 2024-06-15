8283ecb9d95e3bfbee338cd74e3f7d0808157aa1
ensemblersAdmin <admin@ensemblers.com>
2024-06-15 18:08:17 +0200

    JWT

--- App.js
+++ App.js
@@ -10,3 +10,9 @@ export default () => {
     </AuthProvider>
   );
 };
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOGMzYTI3ODQtYjdhZC00YjYyLTg5MTYtZTRmNTBjYjJkZTE4IiwidXNlcl9lbWFpbCI6InVzZXIxQGVtYWlsLmNvbSJ9.
+//bFsRay2kt5ysIwd649Ix_l2alZsMGTQX5-Ayy5cF_TU
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMzcxYTliMDEtMzNkNS00ZGU2LTg2MzktODI5NDI0MTE1ZDQ0IiwidXNlcl9lbWFpbCI6InVzZXIyQGVtYWlsLmNvbSJ9.
+//pOZPRcQy3brO5bGVaqpHgvuTWZbluMb1-UlPAawbg9A
--- src/api/ensemblers.js
+++ src/api/ensemblers.js
@@ -3,6 +3,8 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "http://localhost:3000/",
   baseURL: "https://back-end-4pab.onrender.com",
-  header: "Accept: application/json",
-  header: `Authorization: Bearer ${ACCESS_TOKEN_SECRET}`,
+  headers: {
+    Accept: "application/json",
+    // Authorization: `Bearer ${ACCESS_TOKEN_SECRET}`,
+  },
 });
