9b0e00caf03631ea3cb742e979c648c5a6e3d929
ensemblersAdmin <admin@ensemblers.com>
2024-06-15 15:04:26 +0200

    Update ensemblers.js

--- src/api/ensemblers.js
+++ src/api/ensemblers.js
@@ -3,4 +3,6 @@ import axios from "axios";
 export default axios.create({
   // baseURL: "http://localhost:3000/",
   baseURL: "https://back-end-4pab.onrender.com",
+  header: "Accept: application/json",
+  header: `Authorization: Bearer ${ACCESS_TOKEN_SECRET}`,
 });
