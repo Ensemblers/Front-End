fb2634784911acee798764758d8f6e561bff3c34
ensemblersAdmin <admin@ensemblers.com>
2024-06-16 11:29:11 +0200

    o

---
+++ Ensemblers Demo/Ensemblers Demo/Ensemblers_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ensemblers_DemoApp.swift
+//  Ensemblers Demo
+//
+//  Created by Jeffrey Hoffmann on 16/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Ensemblers_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
