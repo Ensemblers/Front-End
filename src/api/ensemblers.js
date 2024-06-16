af8239de77f8876336289f7a2b5fa24e76d3df86
ensemblersAdmin <admin@ensemblers.com>
2024-06-16 08:40:43 +0200

    Server connection

--- src/api/ensemblers.js
+++ src/api/ensemblers.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "http://localhost:3000/",
   baseURL: "https://back-end-4pab.onrender.com",
-  headers: {
-    Accept: "application/json",
-    // Authorization: `Bearer ${ACCESS_TOKEN_SECRET}`,
-  },
 });
